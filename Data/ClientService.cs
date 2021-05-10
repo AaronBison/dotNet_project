3f8889d78e620dbce814ce5fa07e748f3b714b3e
AaronBison <37123069+AaronBison@users.noreply.github.com>
2021-05-10 13:47:56 +0300

    Initial commit

---
+++ Data/ClientService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotNet_project.Data
+{
+    public class ClientService : IClientService
+    {
+        private List<Client> clients = new List<Client>
+        {
+            new Client
+            {
+                Id = Guid.NewGuid(),
+                Name = "AaronBison"
+            },
+            new Client
+            {
+                Id = Guid.NewGuid(),
+                Name = "RailGummy"
+            }
+        };
+        public List<Client> GetClients()
+        {
+            return clients;
+        }
+    }
+}
---
+++ Data/IClientService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace dotNet_project.Data
+{
+    interface IClientService
+    {
+        List<Client> GetClients();
+    }
+}
